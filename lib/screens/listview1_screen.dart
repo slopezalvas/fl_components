09f0105e73d037366700b1194f82f73cc983a2cb
slopez <slopezalvas@gmail.com>
2022-09-08 16:13:02 -0300

    Se crea una list view

---
+++ lib/screens/listview1_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ListView1Screen extends StatelessWidget {
+
+  final options = const ['Megaman', 'Metal Gear', 'Super Smash', 'Final Fantasy'];
+
+  const ListView1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Listview Tipo 1'),
+      ),
+      body: ListView(
+        children: [
+
+          ...options.map((e) => ListTile(
+            title: Text(e),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            )
+          ).toList()
+
+          
+        ],
+      )
+    );
+  }
+}
