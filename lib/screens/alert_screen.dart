bc4ea0d3aa26b66c3450752ae30cd39837636a64
slopez <slopezalvas@gmail.com>
2022-09-20 17:48:41 -0300

    Mostrar una alerta

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,31 +1,55 @@
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
-   
   const AlertScreen({Key? key}) : super(key: key);
-  
+
+  void displayDialog(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            elevation: 5,
+            title: const Text('Titulo'),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            content: Column(mainAxisSize: MainAxisSize.min, children: const [
+              Text('Este es el contenido de la alerta'),
+              SizedBox(
+                height: 10,
+              ),
+              FlutterLogo(
+                size: 100,
+              )
+            ]),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancelar'))
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Alert Screen'),
         elevation: 0,
       ),
       body: Center(
-         child: ElevatedButton(
-          child: const Padding(
-            padding:  EdgeInsets.symmetric( horizontal:  20, vertical: 15),
-          child:  Text('Mostrar alerta', style: TextStyle(fontSize: 20)),
-          
-          ),
-          onPressed: () {}, 
-        )
-      ),
+          child: ElevatedButton(
+              onPressed: () => displayDialog(context),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                child: Text('Mostrar alerta', style: TextStyle(fontSize: 20)),
+              ))),
       floatingActionButton: FloatingActionButton(
-        child: const Icon (Icons.close),
+        child: const Icon(Icons.close),
         onPressed: () {
           Navigator.pop(context);
-      },),
+        },
+      ),
     );
   }
 }
