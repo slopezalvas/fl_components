afdae696ba069f7a42601caf4f63de8666b15731
slopez <slopezalvas@gmail.com>
2022-09-27 17:04:42 -0300

    TextFormField

---
+++ lib/screens/inputs_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class InputsScreen extends StatelessWidget {
+   
+  const InputsScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text('Inputs y Forms'),
+      ),
+      body:SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20,  vertical: 10),
+          child: Column(
+            children: [
+
+              TextFormField(
+                autofocus: true,
+                initialValue: 'Test',
+                textCapitalization: TextCapitalization.words,
+                onChanged: (value) {
+                  print('value:  $value');
+                },
+              )
+
+            ]
+          ),
+        ),
+      )
+    );
+  }
+}
