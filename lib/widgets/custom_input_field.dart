9376551352bc4c330b3a8d00ef8e6996d52d0824
slopez <slopezalvas@gmail.com>
2022-10-04 16:43:01 -0300

    Email y Password fields

--- lib/widgets/custom_input_field.dart
+++ lib/widgets/custom_input_field.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 class CustomInputField extends StatelessWidget {
-
   final String? hintText;
   final String? labelText;
   final String? helperText;
   final IconData? icon;
   final IconData? suffixIcon;
-  
+  final TextInputType? keyboardType;
+  final bool obscureText;
+
   const CustomInputField({
-    Key? key, this.hintText, this.labelText, this.helperText, this.icon, this.suffixIcon,
+    Key? key,
+    this.hintText,
+    this.labelText,
+    this.helperText,
+    this.icon,
+    this.suffixIcon,
+    this.keyboardType,
+    this.obscureText = false,
   }) : super(key: key);
 
   @override
@@ -18,11 +26,13 @@ class CustomInputField extends StatelessWidget {
       autofocus: true,
       initialValue: '',
       textCapitalization: TextCapitalization.words,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
       onChanged: (value) {
         print('value:  $value');
       },
       validator: (value) {
-        if ( value == null) return 'Este campo es requerido';
+        if (value == null) return 'Este campo es requerido';
         return value.length < 3 ? 'Minimo de 3 letras' : null;
       },
       autovalidateMode: AutovalidateMode.onUserInteraction,
@@ -30,9 +40,8 @@ class CustomInputField extends StatelessWidget {
         hintText: hintText,
         labelText: labelText,
         helperText: helperText,
-        suffixIcon: suffixIcon == null ? null : Icon (suffixIcon),
-        icon: icon == null ? null : Icon (icon),
-
+        suffixIcon: suffixIcon == null ? null : Icon(suffixIcon),
+        icon: icon == null ? null : Icon(icon),
       ),
     );
   }
